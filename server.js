c12d34fc3a4bfd2cf36ae8ef52327c3fcc95d1a5
joeco fong <joecofg@gmail.com>
2020-02-07 14:16:19 -0800

    minor changes

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ var express = require('express'),
     app     = express();
     
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 app.get('/', function (req, res) {
   res.send('Hello')
