3bcbca38a1212e0c4d78389ae2dbd6ce78a28d71
joeco fong <joecofg@gmail.com>
2020-02-07 13:54:20 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+
+
+app.get('/', (req, res) => {
+  res.send("Testing deployment")
+})
+
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
