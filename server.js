4625dd7b1384d97625248e05675ffe98ffe7ea24
joeco fong <joecofg@gmail.com>
2020-02-07 14:22:34 -0800

    added react

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 //  OpenShift sample Node application
 var express = require('express'),
     app     = express();
+    path    = require('path');
     
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
-
-app.get('/', function (req, res) {
-  res.send('Hello')
-});
 
 // error handling
 app.use(function(err, req, res, next){
@@ -15,6 +12,11 @@ app.use(function(err, req, res, next){
   res.status(500).send('Something bad happened!');
 });
 
+
+app.use(express.static(path.join(__dirname, './views/build')));
+app.get('*', (req, res) => {
+  res.sendfile(path.join(__dirname = './views/build/index.html'));
+});
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
