99ddba3f57d1dc9760582643806efa955d77b51b
joeco fong <joecofg@gmail.com>
2020-02-07 14:28:38 -0800

    ignore some stupdi code

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(function(err, req, res, next){
 });
 
 
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, './temp')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname = './build/index.html'));
+  res.sendFile(path.join(__dirname = './temp/index.html'));
 });
 
 app.listen(port, ip);
---
+++ temp/precache-manifest.b00202bcf8166c37b709ada6d4d83e72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66cc33862325523a66f1c5eeceef1d8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aee602938171d0f52f7b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4e4704abf6123a1ae1b7",
+    "url": "/static/js/2.8341e743.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8341e743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aee602938171d0f52f7b",
+    "url": "/static/js/main.f37d1885.chunk.js"
+  },
+  {
+    "revision": "1ba70dd42600215a279a",
+    "url": "/static/js/runtime-main.ad635574.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
